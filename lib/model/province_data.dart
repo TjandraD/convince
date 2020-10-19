300ec6e752e34a152742562656c81562546bb883
TjandraD <tjandradarmo@protonmail.com>
2020-10-19 20:09:20 +0700

    Move list from the main screen

---
+++ lib/model/province_data.dart
@@ -0,0 +1,74 @@
+import 'province.dart';
+
+List<Province> provinceList = [
+  Province(
+    logoFile: 'logo_dki.png',
+    name: 'DKI Jakarta',
+    positive: 48393,
+    recovered: 36383,
+    death: 1317,
+  ),
+  Province(
+    logoFile: 'logo_jatim.png',
+    name: 'Jawa Timur',
+    positive: 36342,
+    recovered: 28731,
+    death: 2608,
+  ),
+  Province(
+    logoFile: 'logo_jateng.png',
+    name: 'Jawa Tengah',
+    positive: 15852,
+    recovered: 10329,
+    death: 1096,
+  ),
+  Province(
+    logoFile: 'logo_jabar.png',
+    name: 'Jawa Barat',
+    positive: 13045,
+    recovered: 6718,
+    death: 283,
+  ),
+  Province(
+    logoFile: 'logo_sulsel.png',
+    name: 'Sulawesi Selatan',
+    positive: 12746,
+    recovered: 9846,
+    death: 373,
+  ),
+  Province(
+    logoFile: 'logo_kalsel.png',
+    name: 'Kalimantan Selatan',
+    positive: 8903,
+    recovered: 6889,
+    death: 373,
+  ),
+  Province(
+    logoFile: 'logo_sumut.png',
+    name: 'Sumatera Utara',
+    positive: 7832,
+    recovered: 4733,
+    death: 342,
+  ),
+  Province(
+    logoFile: 'logo_bali.png',
+    name: 'Bali',
+    positive: 6549,
+    recovered: 5225,
+    death: 128,
+  ),
+  Province(
+    logoFile: 'logo_kaltim.png',
+    name: 'Kalimantan Timur',
+    positive: 5277,
+    recovered: 3018,
+    death: 225,
+  ),
+  Province(
+    logoFile: 'logo_sumsel.png',
+    name: 'Sumatera Selatan',
+    positive: 4786,
+    recovered: 3440,
+    death: 283,
+  ),
+];
